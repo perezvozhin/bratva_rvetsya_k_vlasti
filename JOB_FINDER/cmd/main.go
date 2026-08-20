a6bc44e7ed5ce6cad74ecba80e3b036bd931d974
Vladislav Turbin <228768837+sparxfort1ano@users.noreply.github.com>
2026-08-20 21:26:08 +0300

    test server

--- JOB_FINDER/cmd/main.go
+++ JOB_FINDER/cmd/main.go
@@ -1,6 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
 
 	//инит сервера
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "yo world")
+	})
+
+	fmt.Println("starting server :8080")
+	http.ListenAndServe(":8080", nil)
 }
