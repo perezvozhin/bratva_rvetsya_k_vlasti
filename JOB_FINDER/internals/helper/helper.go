1b23fc46bff973d03b59209845f4c0c436ee6c00
rezzoy222 <kinortd@gmail.com>
2026-08-21 17:34:00 +0300

    функция checkdirectory, проверяющая есть ли файлы в директории

--- JOB_FINDER/internals/helper/helper.go
+++ JOB_FINDER/internals/helper/helper.go
@@ -1,7 +1,22 @@
 package helper
 
-import "fmt"
+import (
+	"os"
+)
 
-func Helper(a string) {
-	fmt.Println(a)
+func Helper(a string) string {
+	return ""
+}
+
+func CheckDirectory(path string) string {
+	files, err := os.ReadDir(path)
+
+	if err != nil {
+		return "empty"
+	}
+
+	if len(files) == 0 {
+		return "empty"
+	}
+	return "full"
 }
